5f7147fcd99f4a7df55a4791d8dca38eaeb6ec73
Pedro Cardozo <p-cardozo@hotmail.com>
2022-01-19 09:18:18 -0300

    Adding TypeOf Operator

---
+++ src/advanced/typeofTypeOperator/index.ts
@@ -0,0 +1,13 @@
+const center = {
+    x: 0,
+    y: 0,
+    z: 0,
+};
+
+type CenterPoint = typeof center;
+
+const simetricCenter = {
+    x: center.x + 1,
+    y: center.y + 1,
+    z: center.z + 1,
+};
